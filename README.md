25ec878ee318ea8e7689cc9e9cff94d5be076547
Raphael Cohn <raphael.cohn@stormmq.com>
2016-05-17 07:53:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+[](This file is part of threading-support. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/threading-support/master/COPYRIGHT. No part of threading-support, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.)
+[](Copyright © 2016 The developers of threading-support. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/threading-support/master/COPYRIGHT.)
+
+# threading-support
+
+[![Clippy Linting Result](https://clippy.bashy.io/github/lemonrock/threading-support/master/badge.svg?style=plastic)](https://clippy.bashy.io/github/lemonrock/threading-support/master/log) [![](https://img.shields.io/badge/Code%20Style-rustfmt-brightgreen.svg?style=plastic)](https://github.com/rust-lang-nursery/rustfmt#configuring-rustfmt)
+
+[threading-support] is a rust crate.
+
+
+## Licensing
+
+The license for this project is MIT.
+
+[threading-support]: https://github.com/lemonrock/threading-support "threading-support GitHub page"
---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+// This file is part of rust1. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/rust1/master/COPYRIGHT. No part of rust1, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+// Copyright © 2016 The developers of rust1. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/rust1/master/COPYRIGHT.
+
+use std::thread;
+use std::thread::JoinHandle;
+use std::thread::Builder;
+use std::time::Duration;
+
+pub const UsefulStackSize: usize = 65536;
+
+pub fn sleepForSeconds(seconds: u64)
+{
+	let duration = Duration::new(seconds, 0);
+	thread::sleep(duration);
+}
+
+pub fn sleepForMilliseconds(milliseconds: u64)
+{
+	let seconds = milliseconds / 1000;
+	let nanoseconds = (milliseconds % 1000) * 1_000_000;
+	let duration = Duration::new(seconds, nanoseconds as u32);
+	thread::sleep(duration);
+}
+
+pub fn sleepForNanoseconds(nanoseconds: u32)
+{
+	let duration = Duration::new(0, nanoseconds);
+	thread::sleep(duration);
+}
+
+pub fn spawnThread<F, T>(name: String, stackSize: usize, f: F) -> JoinHandle<T>
+	where F: FnOnce() -> T, F: Send + 'static, T: Send + 'static,
+{
+	return Builder::new()
+		.name(name)
+		.stack_size(stackSize)
+		.spawn(f)
+		.unwrap();
+}
